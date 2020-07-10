6e4c289e43d658446bfedecd86eb6c6e9d214363
wchamber01 <williamchambers@gmail.com>
2020-07-09 21:01:20 -0400

    caesar and markov

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,43 @@
+import string
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+# import the message
+txt = open('./applications/crack_caesar/ciphertext.txt').read()
 
+cipher_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+               'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+cipher = {i: cipher_list[i] for i in range(0, len(cipher_list))}
+
+dictionary = {}
+
+alphabet = string.ascii_uppercase
+
+for character in txt:
+    for alpha in alphabet:
+        # if character.isalpha(): --> doesn't work for this scenario
+        if character in alpha and character not in dictionary:
+            # dictionary[character] = 1 if character not in dictionary else dictionary[character] + 1
+            dictionary[character] = 1
+    else:
+        if character in dictionary:
+            dictionary[character] += 1
+
+# sort d by highest freq to lowest
+sorted_dict = dict(
+    sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+
+# loop through sorted_dict and replace all values with corresponding letter from cipher
+for index, key in enumerate(sorted_dict):
+    if index in cipher:
+        sorted_dict[key] = cipher[index]
+
+code = ''
+for i in txt:
+    if i in alphabet:
+        # print(i)
+        code += sorted_dict[i]
+    else:
+        code += i
+print(code)
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,8 +1,66 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("./applications/markov/input.txt") as f:
     words = f.read()
+    words = words.split()
+    # print(words)
+following = {}
+
+prev = None
+
+for word in words:
+    if prev is not None:
+        # Make an empty list for the first entries
+        if prev not in following:
+            following[prev] = []
+
+        # Add word to the list of those that are following
+        following[prev].append(word)
+
+    prev = word
+
+# Words that we can start a sentence with
+
+# We'll get clever and look for words where either the first letter is a
+# capital, or the second is (presumably following a quote):
+
+
+def is_good_start(x): return x[0].isupper() or len(x) > 1 and x[1].isupper()
+# if word[0](i.e. first letter of the word) is uppercase or length of word is
+# greater than 1 AND word[1](i.e. character 2) is upper
+# then add word to is_good_start
+
+
+start_words = [w for w in following.keys() if is_good_start(w)]
+# if word in following.keys() and word in is_good_start
+# then start_words = word
+
+
+# Print a number of paragraphs (five in this case)
+for _ in range(5):
+
+    # Choose the starting word at random from start_words
+    word = random.choice(start_words)
+
+    stopped = False  # Initialize stopped to False
+    stop_punctuation = ".!?"  # Initalize stop punctuation on any of these characters
+
+    while not stopped:
+        # print single word from our list and follow printed word with a space
+        print(word, end=" ")
+
+        if word[-1] in stop_punctuation or len(word) > 1 and word[-2] in stop_punctuation:
+            # if last character of word is punctuation or second to last character of word is punctiation
+            # then STOP
+            stopped = True
+        else:
+            # Follow to the next word in the chain
+            next_words = following[word]
+            word = random.choice(next_words)
+
+    print("\n")
+
 
 # TODO: analyze which words can follow other words
 # Your code here
@@ -10,4 +68,3 @@ with open("input.txt") as f:
 
 # TODO: construct 5 random sentences
 # Your code here
-
